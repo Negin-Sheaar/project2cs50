e8a91eace911b8d7b90c4ac14b1562913983d5d5
negin-sheaar <ngnjv53@egmail.com>
2020-05-26 12:58:40 -0500

    secondcommit

--- project2/application.py
+++ project2/application.py
@@ -1,18 +1,18 @@
-
 import os
-from flask import Flask, render_template, jsonify, url_for, session, redirect
-from flask_socketio import SocketIO, emit, send
-from collections import defaultdict
+import requests
+
+from flask import Flask, jsonify, render_template, request
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
-users=[]
+
 @app.route("/")
 def index():
-	return render_template("index.html", users=users)
+    return render_template("index.html")
 
-@socketio.on("login")
-def login(data):
-     users.append(data["users"])
-     emit("loadMsgPage", {"users":users} ,broadcast=True)
+@socketio.on("submit user")
+def vote(data):
+    users = data["users"]
+    emit("announce user", {"users": users}, broadcast=True)
--- project2/static/index.js
+++ project2/static/index.js
@@ -1,26 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-//when DOM is loaded
-document.addEventListener('DOMContentLoaded', ()=>{
-
-
-			// Connect to websocket
+    // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-    socket.on('connect', ()=>{
-    	//load home page
-      document.querySelector('#submituser').onclick  = () => {
-                  const user = document.querySelector('#displayName').value;
-                //  document.querySelector('#loginhomepage').style.display= "none";
-                //  console.log(user);
-                  socket.emit('login', {'users': user});
+    // When connected, configure buttons
+    socket.on('connect', () => {
 
-		};
-  });
-		//if user already registered
+        // Each button should emit a "submit vote" event
+        document.querySelectorAll('button').forEach(button => {
+            button.onclick = () => {
+                const users = document.querySelector('#displayName').value;
+                socket.emit('submit user', {'users': users});
+            };
+        });
+    });
 
-    socket.on('loadMsgPage', data=>{
-      const li = document.createElement('li');
+    // When a new vote is announced, add to the unordered list
+    socket.on('announce user', data => {
+        const li = document.createElement('li');
         li.innerHTML = `Vote recorded: ${data.users}`;
-        document.querySelector('#usernames').append(li);
-    	});
+        document.querySelector('#users').append(li);
+    });
 });
