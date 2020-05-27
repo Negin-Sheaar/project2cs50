138c4b938ebf443da370d07a19433f8a954fe3de
negin-sheaar <ngnjv53@egmail.com>
2020-05-27 13:58:48 -0500

    users and msgcomplete

--- project2/application.py
+++ project2/application.py
@@ -3,17 +3,22 @@ import requests
 
 from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
-
+users=[]
+msg=[]
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    
-    return render_template("index.html")
+
+    return render_template("index.html",users=users,msg=msg)
 
 @socketio.on("submit user")
 def vote(data):
     users = data["users"]
     emit("announce user", {"users": users}, broadcast=True)
+@socketio.on("submit msg")
+def submitmsg(data):
+    msg = data["msg"]
+    emit("announce msg", {"msg": msg}, broadcast=True)
--- project2/static/index.js
+++ project2/static/index.js
@@ -9,20 +9,36 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Each button should emit a "submit vote" event
         document.querySelector('#submituser').onclick = () => {
-                const users = document.querySelector('#displayName').value;
+                const user = document.querySelector('#displayName').value;
+                localStorage.setItem('users', user);
                 document.querySelector('#loginhomepage').style.display="none";
                 document.querySelector('#loginhomepage').remove;
                 document.body.style.backgroundImage="none";
                 document.querySelector('#msgbody').style.display="block";
                 document.querySelector('#grad1').style.background = "red";
+                const users = localStorage.getItem('users');
                 socket.emit('submit user', {'users': users});
+                return false;
             };
+//when user send msg
+            document.querySelector('#sendmsg').onclick = () => {
+                    const msg = document.querySelector('#msgbox').value;
+                    localStorage.setItem('msg', msg);
+                    const users = localStorage.getItem('users');
+                    socket.emit('submit msg', {'msg': msg ,'users':users});
+                    return false;
+                };
           });
 
-    // When a new vote is announced, add to the unordered list
+    // When a new user is connected
     socket.on('announce user', data => {
         const li = document.createElement('li');
         li.innerHTML = `  ${data.users}`;
         document.querySelector('#users').append(li);
+    });
+    socket.on('announce msg', data => {
+        const li = document.createElement('li');
+        li.innerHTML = `  ${data.msg}`;
+        document.querySelector('#msgs').append(li);
     });
 });
